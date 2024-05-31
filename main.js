18c7f28027c08149a415571ae37700c1c7de1b49
Shohag Ahmed <shohag.sub.co@gmail.com>
2024-05-31 11:45:28 +0600

    I am learning array methods

--- main.js
+++
@@ -1,7 +0,0 @@
-let one = ["Shohag", "Sajib", "Raju", "Sahaparan"];
-let two = ["bancharampur", "dhaka", "khulna", "shylet"];
-let addresss = "dinajpur";
-
-let result = one.concat(addresss, two);
-
-console.log(result.copyWithin(3, 5, 8));
---
+++ methods/array-methods.js
@@ -0,0 +1,45 @@
+// Arrays
+let users = ["Shohag Ahmed", "Sahaparan", "Sajib Ahmed", "Jakir Ahmed"];
+
+let userDetails = {
+    "Shohag Ahmed": {
+        address: "Bancharampur",
+        phone: "01400901280",
+        living: "Bancharampur",
+        profession: "none",
+    },
+    Sahaparan: {
+        address: "Bancharampur",
+        phone: "",
+        living: "Bancharampur",
+        profession: "student",
+    },
+    "Sajib Ahmed": {
+        address: "Dhaka",
+        phone: "01367474743",
+        living: "Dhaka",
+        profession: "computer oparator",
+    },
+    "Jakir Ahmed": {
+        address: "Khulna",
+        phone: "01367474743",
+        living: "Saudie Arabia",
+        profession: "unknown",
+    },
+};
+
+let numbers = [
+    322, 336, 2, 734, 83, 24, 8, 236, 7451, 843, 83, 13, 763, 8742, 74,
+];
+
+export let countries = [
+    "Bangladesh",
+    "India",
+    "Pakistan",
+    "USA",
+    "United Kingdom",
+    "Russia",
+    "Nepal",
+];
+
+console.log(countries);
